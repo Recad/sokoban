23bc74f8059e05b8cf24ab53dac0723da98c1bc3
juancho270 <juan.viteri@correounivalle.edu.co>
2019-07-13 12:40:05 -0500

    lectura de un tablero

---
+++ sokoban.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+import string
+import os
+
+tablero_sp = {}
+tablero_cp = {}
+def leerTablero():
+	ruta = os.getcwd()
+	ruta = ruta + "/nivel4.txt"
+	contador = -1
+	contador2 = -1
+	g = open(ruta , "r")
+	for linea in g.readlines():
+		contador = contador + 1
+		tablero_sp[contador] = []
+		for letra in linea:
+			if letra != '\n':
+				tablero_sp[contador].append(letra)
+	posiciones(tablero_sp)
+
+def posiciones(mapa):
+	contador = 0
+	for key in mapa.keys():
+		if mapa[key][1] == ',':
+			if contador == 0:
+				contador = contador + 1
+				mapa[int(mapa[key][0])][int(mapa[key][2])] = "J"
+			else:
+				mapa[int(mapa[key][0])][int(mapa[key][2])] = "C"
+				print(mapa[key][2])
+	tablero_cp = mapa
+	print(tablero_sp)
+
+leerTablero()
